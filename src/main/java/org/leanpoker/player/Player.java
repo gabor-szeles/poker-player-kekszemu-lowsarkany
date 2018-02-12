46f0dc254639840a2a96d32922245da8baca4db5
Boglarka Nagy <thebogosz@gmail.com>
2018-02-12 10:34:29 +0100

    minRaise return

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 import java.util.Map;
 
@@ -9,8 +11,10 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        JsonObject jobject = request.getAsJsonObject();
+        int minRaise = jobject.getAsJsonPrimitive("minimum_raise").getAsInt();
 
-        return 100;
+        return minRaise;
     }
 
     public static void showdown(JsonElement game) {
