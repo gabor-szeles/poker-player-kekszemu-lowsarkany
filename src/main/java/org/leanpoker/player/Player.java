72c12845c939f5ee55be541a589fdf0a89fb3de9
Boglarka Nagy <thebogosz@gmail.com>
2018-02-12 11:27:17 +0100

    getting cards and stacks

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,9 +1,12 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
@@ -13,6 +16,26 @@ public class Player {
     public static int betRequest(JsonElement request) {
         JsonObject jobject = request.getAsJsonObject();
         int minRaise = jobject.getAsJsonPrimitive("minimum_raise").getAsInt();
+        int pot = jobject.getAsJsonPrimitive("pot").getAsInt();
+        int playerStack;
+        JsonArray playerCards;
+        List<Integer> enemyStacks = new ArrayList<>();
+        JsonArray players = jobject.getAsJsonArray("players");
+        JsonArray communityCards = jobject.getAsJsonArray("community_cards");
+        // get stacks
+        for (int i = 0; i < players.size(); i++) {
+            int stack = players.get(i).getAsJsonPrimitive("stack").getAsInt();
+            if(players.get(i).getAsString("name").equals("Kekszemu Lowsarkany")){
+                playerStack = stack;
+                playerCards = players.get(i).getAsJsonArray("hole_cards");
+            } else{
+                enemyStacks.add(stack);
+            }
+        }
+
+        //check cards
+
+
 
         return minRaise;
     }
